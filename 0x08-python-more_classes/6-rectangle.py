6748537ae0fe9054d6276b41cb2d197efa314026
maggywairigu <wairigumaina2016@gmail.com>
2023-07-06 04:49:57 -0700

    file 6 pycode validation bug fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -80,8 +80,8 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)+ ", " +
-        str(self.__height) + ")"
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
         return (rect)
 
     def __del__(self):
