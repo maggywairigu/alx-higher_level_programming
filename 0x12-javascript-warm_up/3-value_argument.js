09dc5771c4185012b135d8948ec3d125bc0ddf97
maggywairigu <wairigumaina2016@gmail.com>
2023-09-12 08:57:14 -0700

    file 3 updates

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const numberOfArgs = args.length;
+let numberOfArgs = 0;
+for (arg in args){
+    numberOfArgs++;
+}
 if (numberOfArgs === 0) {
   console.log('No argument');
 } else {
