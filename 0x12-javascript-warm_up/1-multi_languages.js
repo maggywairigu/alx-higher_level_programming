f92b87b395d0e205a2976f976de6feafed7e814f
maggywairigu <wairigumaina2016@gmail.com>
2023-09-12 08:07:07 -0700

    file 1 added

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let i = 0;
+while (i < 3) {
+  if (i === 0) {
+    console.log('C is fun');
+  } else if (i === 1) {
+    console.log('Python is cool');
+  } else {
+    console.log('JavaScript is amazing');
+  }
+  i++;
+}
