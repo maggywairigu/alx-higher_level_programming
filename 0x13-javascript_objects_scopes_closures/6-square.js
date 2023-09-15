28c010d46feb94c837490b237d53f6856f80a8e5
maggywairigu <wairigumaina2016@gmail.com>
2023-09-15 12:34:06 -0700

    file 6 bug fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends require('./5-square.js') {
       for (let i = 0; i < this.height; i++) {
         let row = '';
         for (let j = 0; j < this.width; j++) {
-          row += 'C';
+          row += c;
         }
         console.log(row);
       }
