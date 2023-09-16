0a54630f155313906ed1ac0b0b7255dd42b0b7e7
maggywairigu <wairigumaina2016@gmail.com>
2023-09-16 07:28:20 -0700

    file 6 updated

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
 -- Lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- Execute: cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT * from first_table;
+SELECT * FROM first_table;
