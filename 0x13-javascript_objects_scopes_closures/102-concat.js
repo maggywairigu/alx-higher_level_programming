53536673b1307b40983a9df44f59a787b5163561
maggywairigu <wairigumaina2016@gmail.com>
2023-09-16 00:53:37 -0700

    file 102 added

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+// Read the content of the first source file
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Read the content of the second source file
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Concatenate the contents of the two files
+    const concatenatedData = data1 + data2;
+
+    // Write the concatenated data to the destination file
+    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(data1);
+      console.log(data2);
+    });
+  });
+});
