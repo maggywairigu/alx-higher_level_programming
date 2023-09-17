94e55081eea97f8371fc5d456541b2b380e97dee
maggywairigu <wairigumaina2016@gmail.com>
2023-09-17 00:55:11 -0700

    file 101 added

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+-- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temparatures GROUP BY city ORDER BY avg_temp DESC;
