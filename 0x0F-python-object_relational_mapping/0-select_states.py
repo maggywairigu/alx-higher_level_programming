d3c0768a86214e4f8953bdef4e4956a21b36d63d
maggywairigu <wairigumaina2016@gmail.com>
2023-09-22 12:04:28 -0700

    0-select_states.py updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -19,8 +19,7 @@ if __name__ == "__main__":
                              port=3306,
                              user=username,
                              passwd=password,
-                             db=database
-        )
+                             db=database)
         """create a cursor object"""
         cursor = db.cursor()
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
