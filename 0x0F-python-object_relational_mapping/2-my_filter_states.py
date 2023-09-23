8cdede262fbedc787c077272744aaf02724ce66e
maggywairigu <wairigumaina2016@gmail.com>
2023-09-23 11:50:29 -0700

    file 2 documentation updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name searched (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+You must use format to create the SQL query with the user input
+Results must be sorted in ascending order by states.id
+"""
 import MySQLdb
 import sys
 
