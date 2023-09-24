b99e1002678e38b7c8f18079bd869c5d73a7dc2b
maggywairigu <wairigumaina2016@gmail.com>
2023-09-24 02:33:02 -0700

    1-filter_states.py bug fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -31,8 +31,7 @@ if __name__ == "__main__":
                              db=database)
         """create a cursor object"""
         cursor = db.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
+        cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
         states = cursor.fetchall()
         for state in states:
             print(state)
