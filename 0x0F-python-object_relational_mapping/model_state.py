efe39cc70ca2528f935dbe127c040261096a59ad
maggywairigu <wairigumaina2016@gmail.com>
2023-09-24 09:28:14 -0700

    model_state.py documentation updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State
-and an instance Base = declarative_base():
+Contains the class definition of a State and an instance Base = declarative_base():
+
 State class:
-inherits from Base Tips
-links to the MySQL table states
-class attribute id that represents a column of
-an auto-generated, unique integer, can’t be null and is a primary key
-class attribute name that represents a column of a string with
-maximum 128 characters and can’t be null
-You must use the module SQLAlchemy
-Your script should connect to a MySQL server running on localhost at port 3306
-WARNING: all classes who inherit from Base must be
-imported before calling Base.metadata.create_all(engine)
+- Inherits from Base
+- Links to the MySQL table states
+- Class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+- Class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+
+You must use the module SQLAlchemy.
+Your script should connect to a MySQL server running on localhost at port 3306.
+WARNING: All classes that inherit from Base must be imported before calling Base.metadata.create_all(engine).
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -24,6 +23,13 @@ Base = declarative_base()
 
 
 class State(Base):
+     """
+    Represents a state in the hbtn_0e_6_usa database.
+
+    Attributes:
+        id (int): An auto-generated, unique integer representing the primary key.
+        name (str): A string representing the name of the state with a maximum length of 128 characters.
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
