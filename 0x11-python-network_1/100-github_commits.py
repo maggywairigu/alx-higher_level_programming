76c39eb58e11351fd33e9554df27609039857614
maggywairigu <wairigumaina2016@gmail.com>
2023-10-01 02:48:12 -0700

    100-github_commits.py added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     from requests import get
     from sys import argv
 
-
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     commits = get(url).json()
     try:
@@ -20,5 +19,5 @@ if __name__ == "__main__":
             print("{}: {}".format(
                 commits[i].get("sha"),
                 commits[i].get("commit").get("author").get("name")))
-    except:
+    except IndexError:
         pass
