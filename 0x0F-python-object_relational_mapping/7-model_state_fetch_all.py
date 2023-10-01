98514d1b0b5e5ce76a25ecadac52e75988e1abc1
maggywairigu <wairigumaina2016@gmail.com>
2023-10-01 06:04:24 -0700

    7-model_state_fetch_all.py updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
 Lists all State objects from the database hbtn_0e_6_usa
-Your script should take 3 arguments: mysql username, mysql password, and database name
+Your script should take 3 arguments:
+mysql username, mysql password, and database name
 You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
+You must import State and Base from model_state
+Your script should connect to a MySQL server
 Results must be sorted in ascending order by states.id
 The results must be displayed as they are in the example below
 Your code should not be executed when imported
@@ -13,24 +14,15 @@ Your code should not be executed when imported
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python list.py <username> <pswd> <db>")
-        sys.exit(1)
-
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                               pool_pre_ping=True)
 
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        states = session.query(State).order_by(State.id).all()
-        for state in states:
-            print(f"{state.id}: {state.name}")
-
-        session.close()
+        for state in session.query(State).order_by(State.id):
+            print("{}: {}".format(state.id, state.name))
