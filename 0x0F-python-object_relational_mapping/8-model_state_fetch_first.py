4e157f8a1b2e5843e254a4d451a0329d9bd46776
maggywairigu <wairigumaina2016@gmail.com>
2023-10-01 06:28:48 -0700

    8-model_state_fetch_first.py up[dated"

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 Prints the first State object from the database hbtn_0e_6_usa
-Your script should take 3 arguments: mysql username, mysql password, and database name
+Your script should take 3 arguments:
+mysql username, mysql password, and database name
 You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
+You must import State and Base from model_state
 Your script should connect to a MySQL server running on localhost at port 3306
 The state you display must be the first in states.id
-You are not allowed to fetch all states from the database before displaying the result
 The results must be displayed as they are in the example below
 If the table states is empty, print Nothing followed by a new line
 Your code should not be executed when imported
@@ -15,28 +15,19 @@ Your code should not be executed when imported
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python list.py <username> <pswd> <db>")
-        sys.exit(1)
+    engine = create_engine("mysql+mqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    first_state = session.query(State).order_by(State.id).first()
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        first_state = session.query(State).order_by(State.id).first()
-
-        if first_state:
-            print(f"{first_name.id}: {first_state.name}")
-        else:
-            print("Nothing")
-
-        session.close()
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
