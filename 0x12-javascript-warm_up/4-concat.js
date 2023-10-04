00d8d0106fdc9119590d09eb29a27792f1c145d7
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2023-10-04 09:52:32 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.slice(2);
-const str1 = args[0];
-const str2 = args[1];
-const result = str1 + ' is ' + str2;
-console.log(result);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const str of strings) {
-  console.log(str);
-}
