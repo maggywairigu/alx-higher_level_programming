b4cca96d98b163a928939f6ae8f40970230149f2
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2023-10-04 09:53:01 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const array = require('./100-data.js');
-const newArray = array.list.map((element, index) => element * index);
-console.log(array.list);
-console.log(newArray);
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-let index = 0;
-exports.logMe = function (item) {
-  console.log(`${index}: ${item}`);
-  index++;
-};
