9b0eb3aa6da1513dd4b20bbd42d0941f947ea504
maggywairigu <wairigumaina2016@gmail.com>
2023-11-14 09:57:49 -0800

    1-multi_languages.js updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'JavaSript is amazing';
-console.log(first);
-console.log(second);
-console.log(third);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
