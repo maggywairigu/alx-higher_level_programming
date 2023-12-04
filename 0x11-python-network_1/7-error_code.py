1133f80442535b28a953dba655d9c859ee68edb7
maggywairigu <wairigumaina2016@gmail.com>
2023-12-04 05:18:30 -0800

    7-error_code.py added

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body of the response.
+"""
+import requests
+from sys import argv
+
+
+def main(argv):
+    """
+    Method that manage urllib.error.HTTPError exceptions and
+    print: Error code: followed by the HTTP status code
+    """
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code == requests.code.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
