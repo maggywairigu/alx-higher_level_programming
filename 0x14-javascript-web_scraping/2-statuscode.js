73308def91b082ff3ef92593dfc221ff8cff5fb0
maggywairigu <wairigumaina2016@gmail.com>
2024-01-03 08:53:37 -0800

    2-statuscode.js updated

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-    if (error){
-        console.error(error);
-    } else {
-        console.log(`code: ${response.statusCode}`)
-    }
-})
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
