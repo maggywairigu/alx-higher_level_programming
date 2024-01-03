639fa234d3dcce0763f82e8fddee09a775aa4221
maggywairigu <wairigumaina2016@gmail.com>
2024-01-03 08:40:42 -0800

    1-writeme.js added

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(filePath, text, 'utf-8', (err) => {
+  if (err) console.log(err);
+});
