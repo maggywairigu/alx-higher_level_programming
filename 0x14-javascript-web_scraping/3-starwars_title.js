bd53bf5e1066e0aadc2611f082cb0f0b893fa305
maggywairigu <wairigumaina2016@gmail.com>
2024-01-03 09:12:57 -0800

    3-starwars_title.js updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,14 +4,14 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-	try {
-            const movieData = JSON.parse(body);
-	    console.log(movieData.title);
-	} catch (parseError) {
-            console.error(parseError)
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log(movieData.title);
+    } catch (parseError) {
+      console.error(parseError);
     }
+  }
 });
