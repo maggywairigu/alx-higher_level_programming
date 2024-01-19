05068af532a37bc307807c1727b69cf9163ccec8
maggywairigu <wairigumaina2016@gmail.com>
2024-01-19 00:44:13 -0800

    0x02-python-import_modules missing files added

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,23 +1,10 @@
- #!/usr/bin/python3
-import importlib.util
-
-def list_module_names(module_file):
-    # Create a spec for the module
-    module_spec = importlib.util.spec_from_file_location("hidden_module", module_file)
-
-    # Load the module from the spec
-    module = importlib.util.module_from_spec(module_spec)
-    module_spec.loader.exec_module(module)
-
-    # Get the names defined in the module
-    module_names = [name for name in dir(module) if not name.startswith("__")]
-
-    return sorted(module_names)
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    module_file = "hidden_4.pyc"
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    module_names = list_module_names(module_file)
-
-    for name in module_names:
-        print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
